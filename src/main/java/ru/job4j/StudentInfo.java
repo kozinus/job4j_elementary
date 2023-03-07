c54af6ec80198c2f3f15b2fb9a50c32eb86c20dd
kozinus <kkozin2403@gmail.com>
2023-03-08 01:10:01 +0300

    4.2. Заголовок в файле java [#159036]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Kirill Kozin");
+        System.out.println("04.04.2003");
+    }
+}
