da9f944c69b2a6a2ebbe34645bbcdd7876367d94
kozinus <kkozin2403@gmail.com>
2023-03-13 13:43:46 +0300

    5.7. Ипотека [#156315]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,8 +3,8 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, double salary, double percent) {
         percent = 1 + percent * 0.01;
-        int years = 1;
-        while ((amount * percent - salary) > 0.0) {
+        int years = 0;
+        while (amount > 0) {
             years++;
             amount = amount * percent - salary;
         }
