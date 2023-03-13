8107c08c64b4eb8971c3a67795357da191e580e6
kozinus <kkozin2403@gmail.com>
2023-03-13 13:42:07 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,13 @@ public class ArrayDefinition {
         System.out.println("Размер массива ages равен = " + ages.length);
         System.out.println("Размер массива surnames равен = " + surnames.length);
         System.out.println("Размер массива prices равен = " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Kirill Kozin";
+        names[1] = "Petr Arsentev";
+        names[2] = "Dimitriy Shaban";
+        names[3] = "Andrej Gikalov";
+        for (int index = 0; index < 4; index++) {
+            System.out.println(names[index]);
+        }
     }
 }
