20c5a0a779add9c55b66532bdc096e047c0644e2
kozinus <kkozin2403@gmail.com>
2023-03-16 17:16:53 +0300

    6.9. String - это массив [#504884]

---
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+
+public class JavaNameValidator {
+    public static boolean isNameValid(String name) {
+        if (name.isEmpty() || !isLowerLatinLetter(name.codePointAt(0))) {
+            return false;
+        }
+        for (int i = 1; i < name.length(); i++) {
+            int code = name.codePointAt(i);
+            if (!(isLowerLatinLetter(code) || isUpperLatinLetter(code) || isSpecialSymbol(code) || Character.isDigit(code))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isSpecialSymbol(int code) {
+        return (code == 36) || (code == 95);
+    }
+
+    public static boolean isUpperLatinLetter(int code) {
+        return code >= 65 && code <= 90;
+    }
+
+    public static boolean isLowerLatinLetter(int code) {
+        return code >= 97 && code <= 122;
+    }
+}
